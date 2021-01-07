c4fe9fe8d5aa3e84555ebbdd30fa2beaae727219
Jotahm <jhmarcianes@gmail.com>
2021-01-07 19:24:53 +0100

    Create variables.py
    Aprendiendo el concepto de variable y sus principales usos

---
+++ variables.py
@@ -0,0 +1,24 @@
+# una variable es un espacio en memoria capaz de almacenar información
+
+# El operador de asignacion es el simbolo = y nos permite asignar un valor a una variable.
+
+numero = 10
+
+print ("numero") #si pongo comillas imprimira el texto encomillado
+
+print (numero) # si no uso comillas, imprime el valor de la variable
+
+# las únicas palabras que no se pueden utilziar para formar variables son las "palabras reservadas" "keywords"
+
+help("keywords")
+
+# existen otras palabras que aunque si se pueden usar, no se recomienda que se haga "builtins"
+
+#dir(__builtins__)
+
+numero1, numero2, numero3 = 1, 2, 3 #variable multiple
+
+print (numero2) # y nos muestra el valor 2
+
+dir() # nos muestra todas las variables guardadas en memoria
+#dell(variable) nos permite liberar el espacio de dicha variable en memoria
