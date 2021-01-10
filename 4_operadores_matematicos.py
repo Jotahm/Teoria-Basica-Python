7d69c7b12b301e24f2f838ac7b0bc33c714d7c01
Jotahm <jhmarcianes@gmail.com>
2021-01-10 18:39:09 +0100

    Añadimos operadores matemáticos
    Una ayuda teórica para el uso de operadores en el lenguaje python

---
+++ 4_operadores_matematicos.py
@@ -0,0 +1,27 @@
+#4
+
+# Los operadores son los distintos signos que el intérprete utiliza para hacer una operación específica
+
+# + - * / % (Al hacer una división / nos devuelve un número flotante. Para que nos devuelva un entero, utilizamos //. Ejemplo 10/2=5.0  ; 10//2=5)
+# ** exponente
+
+# El orden de precedencia es similar al matemático (Paréntesis-exponentes-multiplicación-división-suma-resta)
+
+numero1 = 10
+numero2 = 5
+
+print (numero1 + numero2)
+print (numero1 * numero2)
+print (numero1 / numero2)
+print (numero1 // numero2)
+
+print (float (numero1))
+
+resultado = 12 * 5 + 2 / 3 ** 2
+
+print (resultado)
+
+resultado2 = (12 * 5) + (2 / 3) ** 2
+
+print (resultado2)
+
