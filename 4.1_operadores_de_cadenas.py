daf2b1d0e091f7a5fa4f7deedf791402c7f40156
Jotahm <jhmarcianes@gmail.com>
2021-01-11 22:22:47 +0100

    Create 4.1_operadores_de_cadenas.py
    Combinemos cadenas

---
+++ 4.1_operadores_de_cadenas.py
@@ -0,0 +1,18 @@
+# 4.1
+
+# Operadores para cadenas
+
+"""
++ Operador de concatenación
+* Operador de Repetición
+"""
+
+cadena1= "Hola " #ese espacio hace que las cadenas no aparezcan juntas "HolaMundo"
+cadena2= "Mundo"
+
+cadenas_unidas = cadena1 + cadena2
+print (cadenas_unidas)
+
+cadenas_repetidas = cadena1 * 3 #Hola Hola Hola
+cadenas_repetidas2 = cadena2 * 4 #mundomundomundomundo Sin espacio porque no lo tiene
+print (cadenas_repetidas)
