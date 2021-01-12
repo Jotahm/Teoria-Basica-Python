6dedaef51b43aad638025d76e0a1d4a40801edc7
Jotahm <jhmarcianes@gmail.com>
2021-01-12 21:39:17 +0100

    Añadiendo más operadores
    Operadores de asignación

---
+++ operadores_de_asignacion.py
@@ -0,0 +1,29 @@
+#4.3
+
+# Los operadores de asignación sirven para asignar un valor a una variable
+
+"""
+= Asignación simple -> x = y
++= Asignación suma -> x += y ; significa que x = x + y
+-= Asignación resta -> x -= ; significa que x = x - y
+*= Asignación multiplicación -> x *= ; significa que x = x * y
+**= Asignación exponente -> x **= y ; que significa que x = x ** y
+/= Asignación división -> x /= ; significa que x = x / y
+//= Asignación división entera -> x //= ; significa que x = x // y
+"""
+
+# Podemos asignar varios valores en una sola linea ; numero 1, numero 2, numero3 = 1, 2, 3
+
+numero1 = 1
+numero2 = 2
+numero3, numero4, numero5 = 3, 4, 5
+
+# Asignación suma
+
+numero1= numero1 + numero2 # es igual que numero1 += numero2
+
+# Asignación resta
+
+numero1 -= numero2 # es igual que numero1 = numero1 - numero2
+
+# ASÍ SUCESIVAMENTE CON TODAS LAS OPERACIONES
