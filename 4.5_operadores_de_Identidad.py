4c6fe8bf642ee3ab747e082023d93c13cdb7a649
Jotahm <jhmarcianes@gmail.com>
2021-01-14 10:50:07 +0100

    Operadores de identidad
    Añadimos operadores de identidad.

---
+++ 4.5_operadores_de_Identidad.py
@@ -0,0 +1,17 @@
+#4.5
+
+# OPERADORES DE IDENTIDAD
+# Son aquellos que nos permiten comprobar si un objeto es igual o no a otro objeto
+
+"""
+is -> es igual
+is not -> no es igual
+"""
+
+a = 10
+b = 10
+
+print (a is b) #True
+print (a is not b) #False
+
+print (type (a) is int) #True
