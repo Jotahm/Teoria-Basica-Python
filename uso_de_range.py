b11177efdb8fe84ba685296729c72591cee3a255
Josué Hormeño <76787941+Jotahm@users.noreply.github.com>
2021-01-29 19:44:41 +0100

    Create uso_de_range.py

---
+++ uso_de_range.py
@@ -0,0 +1,6 @@
+x = 27
+numeros_en_rango =[]
+for numero in range(x):
+    numeros_en_rango.append(numero)
+    
+print (numeros_en_rango) #[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26]
